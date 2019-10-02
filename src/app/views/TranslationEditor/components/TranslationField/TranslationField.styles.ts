a6e88a9422b2f636b78f36bd8c405b4b4a2f3205
Denis Masinovic <denis.masinovic@gmail.com>
2019-10-02 21:24:15 +0200

    Switched to raw textarea for performance reasons

---
+++ src/app/views/TranslationEditor/components/TranslationField/TranslationField.styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useTranslationFieldStyles = makeStyles(theme => ({
+  textarea: {
+    display: 'block',
+    width: '100%',
+    height: 35,
+    minHeight: 35,
+    border: 'none',
+    resize: 'vertical',
+    fontFamily: theme.typography.body1.fontFamily,
+    fontSize: theme.typography.body1.fontSize,
+    padding: theme.spacing(1),
+  },
+}));
